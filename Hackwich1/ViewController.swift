6eb2f5e1bd107d2e74977499f14823993cd884b3
CM Student <cmstudent@uhwo-acm-10.local>
2019-01-10 10:17:02 -1000

    Initial Commit

---
+++ Hackwich1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hackwich1
+//
+//  Created by CM Student on 1/10/19.
+//  Copyright © 2019 Shaelyn Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
